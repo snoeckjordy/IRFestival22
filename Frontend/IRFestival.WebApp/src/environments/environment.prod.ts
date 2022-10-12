bc518501f8923977a1419144cf5565a899b75e69
snoeckjordy <jordysnoeckk@hotmail.com>
2022-10-12 15:36:22 +0200

    StaticWebApp

--- Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  festivalName: "IRFestival",
-  apiBaseUrl: "https://????.azurewebsites.net/api/"
+  festivalName: 'IRFestival',
+  apiBaseUrl: 'https://irfestivalapijs.azurewebsites.net/api/settings',
 };
