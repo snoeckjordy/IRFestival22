6e9ffbd7e53c30ceeb8a65a27919eb4b158e19d9
snoeckjordy <jordysnoeckk@hotmail.com>
2022-10-14 15:46:57 +0200

    TrackFix6.0

--- Backend/IRFestival.Api/Program.cs
+++ Backend/IRFestival.Api/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+
 //Storage
 var storageSharedKeyCredential = new StorageSharedKeyCredential(
     builder.Configuration.GetValue<string>("Storage:AccountName"),
