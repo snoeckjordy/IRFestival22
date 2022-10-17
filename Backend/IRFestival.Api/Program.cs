bef12841a5d6c21059f2f548a50c2ba3863588f7
snoeckjordy <jordysnoeckk@hotmail.com>
2022-10-17 08:50:19 +0200

    Program Fix

--- Backend/IRFestival.Api/Program.cs
+++ Backend/IRFestival.Api/Program.cs
@@ -6,21 +6,15 @@ using IRFestival.Api.Data;
 using IRFestival.Api.Options;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
-
+//
 // Add services to the container.
-builder.Services.AddCors();
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Configuration.AddAzureKeyVault(
     new Uri($"https://irfestivalkeyvaultjs.vault.azure.net/"),
-    new DefaultAzureCredential(new DefaultAzureCredentialOptions()));
-builder.Services.Configure<AppSettingsOptions>(builder.Configuration.GetSection("AppSettings"));
+    new DefaultAzureCredential(new DefaultAzureCredentialOptions())
+);
 
-//Storage
 var storageSharedKeyCredential = new StorageSharedKeyCredential(
     builder.Configuration.GetValue<string>("Storage:AccountName"),
     builder.Configuration.GetValue<string>("Storage:AccountKey"));
@@ -30,20 +24,26 @@ builder.Services.AddSingleton(p => new BlobServiceClient(new Uri(blobUri), stora
 builder.Services.AddSingleton(p => storageSharedKeyCredential);
 builder.Services.AddSingleton<BlobUtility>();
 builder.Services.Configure<BlobSettingsOptions>(builder.Configuration.GetSection("Storage"));
- 
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<AppSettingsOptions>(builder.Configuration.GetSection("AppSettings"));
 builder.Services.AddDbContext<FestivalDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-sqlServerOptionsAction: sqlOptions =>
 {
-    sqlOptions.EnableRetryOnFailure(
-        maxRetryCount: 10,
-            maxRetryDelay: TimeSpan.FromSeconds(30),
-            errorNumbersToAdd: null
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 10,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null
             );
-
-}));
+        });
+});
 builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
-
 var app = builder.Build();
 
 app.UseSwagger();
