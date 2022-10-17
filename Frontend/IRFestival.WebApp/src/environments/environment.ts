89a2e42253f2394eadedca7ca969eabfe2138e42
snoeckjordy <jordysnoeckk@hotmail.com>
2022-10-17 13:34:09 +0200

    api url

--- Frontend/IRFestival.WebApp/src/environments/environment.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   festivalName: 'IRFestival - DEV',
-  apiBaseUrl: 'https://localhost:7097/api/',
+  apiBaseUrl: 'https://irfestivalapimjss.azure-api.net/2022/v1/api/',
   connectionstringAppInsights:
     'InstrumentationKey=524e4949-28a3-435e-bf6a-d053985583d9',
 };
